3f7556cd9e28a49683fb716aa5cddaca377013be
Dylan <drossi517@gmail.com>
2021-01-24 15:37:56 -0800

    Day 1 Exercises Done

---
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -0,0 +1,49 @@
+//  Dylan Rossi
+//  1.24.21
+
+//  Day 1 Exercises: 30 Days of Javascript
+
+//Part 1:
+//Comments can make code readable
+
+//Part 2:
+//Welcome to 30 Days of Javascript
+
+//Part 3:
+/*Comments can make code readable,
+    easy to reuse and informative.
+ */
+
+//Part 4: Declaring and assigning variables:
+let name = 'Dylan Rossi'
+let sex = 'male'
+let isTall = true
+let isMarried = false
+let x = null
+let y = 25
+let z = 17.8
+let undefVar
+
+console.log(typeof(name))
+console.log(typeof(sex))
+console.log(typeof(isTall))
+console.log(typeof(isMarried))
+console.log(typeof(x))
+console.log(typeof(y))
+console.log(typeof(z))
+console.log(typeof(undefVar))
+
+let a=2, b=4, c=9, d =5
+let e = 1, f = 2, g = 3, h = 4
+let i = a+b+c+d+e+f+g+h
+
+let newName = 'Dylan J Rossi', isNotMarried = true, country = 'USA', age = 32
+console.log(newName, isNotMarried,isTall, sex, country, age, i)
+let myAge = 33
+let yourAge = 30
+
+console.log('I am', myAge, 'years old.')
+console.log('You are', yourAge, 'years old.')
+
+
+
